f8da350237fb5ca28bec6177d1fc8ea8c1715c02
DarkEmBasSy666 <67989743+jganz6@users.noreply.github.com>
2021-03-30 13:59:21 +0700

    add fix

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,6 +1,5 @@
 const Router = require("express").Router();
 const pingRouter = require("./ping");
-const usersRouter = require("./users");
 const authRouter = require("./auth");
 const classRouter = require("./class");
 Router.use("/ping", pingRouter);
