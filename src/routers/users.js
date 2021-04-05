3abd81626580ea93f68614d3f3d5a679263ccae6
DarkEmBasSy666 <67989743+jganz6@users.noreply.github.com>
2021-04-05 11:03:13 +0700

    add all fix route

--- src/routers/users.js
+++ src/routers/users.js
@@ -2,7 +2,7 @@ const Router = require("express").Router();
 const usersHandler = require("../handlers/users");
 const verified = require("../middlewares/verifyToken");
 Router.get("/", verified, usersHandler.getUsersInfo);
-Router.patch("/update", verified, usersHandler.getUsersInfo);
-Router.delete("/delete", verified, usersHandler.getUsersInfo);
+Router.patch("/update", verified, usersHandler.updateAccount);
+Router.delete("/delete", verified, usersHandler.deleteAccount);
 
 module.exports = Router;
