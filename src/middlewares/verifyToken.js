bca058a6e9775100a236f07c67f2644a4d81bce9
DarkEmBasSy666 <67989743+jganz6@users.noreply.github.com>
2021-04-09 16:09:55 +0700

    fix all backend

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,5 +1,16 @@
 const jwt = require(`jsonwebtoken`);
-
+const allRole = (req, res, next) => {
+  const token = req.header(`auth-token`);
+  if (!token) return res.status(401).send(`Access Denied`);
+  try {
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.user = verified;
+    console.log(req.user);
+    next();
+  } catch (err) {
+    res.status(400).send(`Invalid Token`);
+  }
+};
 const student = (req, res, next) => {
   const token = req.header(`auth-token`);
   if (!token) return res.status(401).send(`Access Denied`);
@@ -35,4 +46,5 @@ const facilitator = (req, res, next) => {
 module.exports = {
   student,
   facilitator,
+  allRole,
 };
--- src/routers/users.js
+++ src/routers/users.js
@@ -2,13 +2,13 @@ const Router = require("express").Router();
 const usersHandler = require("../handlers/users");
 const verified = require("../middlewares/verifyToken");
 const multerUploadImage = require("../middlewares/uploadImages");
-Router.get("/", verified, usersHandler.getUsersInfo);
+Router.get("/", verified.allRole, usersHandler.getUsersInfo);
 Router.patch(
   "/",
-  verified,
+  verified.allRole,
   multerUploadImage.single("image"),
   usersHandler.updateAccount
 );
-Router.delete("/", verified, usersHandler.deleteAccount);
+Router.delete("/", verified.allRole, usersHandler.deleteAccount);
 
 module.exports = Router;
