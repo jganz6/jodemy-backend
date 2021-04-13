d929da1d59e082a885fd68f2f3fb769b53ac0bbe
DarkEmBasSy666 <67989743+jganz6@users.noreply.github.com>
2021-04-13 10:48:57 +0700

    edit message

--- src/handlers/auth.js
+++ src/handlers/auth.js
@@ -32,7 +32,7 @@ const sendOTP = async (req, res) => {
           "<h1 style='font-weight:bold;'>" +
           otp +
           "</h1>" +
-          "<h4>expired in 2 minutes</h4>", // html body
+          "<h3>expired in 2 minutes</h3>", // html body
       };
       transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
