7b67089d57cb277d8774fbe8dc8ee7267923de4f
jganz6 <jodiemanopo28@gmail.com>
2021-05-10 04:58:02 +0700

    add db backups

---
+++ testing.js
@@ -0,0 +1,12 @@
+var daftarNama = ["jodie", "jenie", "kyntil7", "kali", "rocky"];
+//////////
+var i = 0;
+while (i < daftarNama.length) {
+  //mulai
+  print(daftarNama[i]);
+  i = i + 1;
+  //akhir
+}
+function print(x) {
+  return console.log(x);
+}
