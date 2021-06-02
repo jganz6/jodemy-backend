6b013f9f61e9ef4866144fb25821f2deea17c2ba
jganz6 <jodiemanopo28@gmail.com>
2021-06-03 00:20:33 +0700

    init socket.io

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 require("dotenv").config();
 const logger = require("morgan");
 const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
 const cors = require("cors");
 const app = express();
-const Router = require("./src/routers/router");
-app.use(cors());
-app.listen(process.env.PORT, () => {
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: ["http://localhost:8000"],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["auth-token"],
+  },
+});
+io.on("connection", (socket) => {
+  console.log(`${socket.id} has joined`);
+  socket.on("send-message", (body, room, cb) => {
+    console.log(`incoming from ${socket.id}`);
+    cb({ status: true });
+
+    if (room) {
+      socket.to(room).emit("message-received", body);
+    } else {
+      socket.broadcast.emit("message-received", body);
+    }
+  });
+  socket.on("join-room", (room, cb) => {
+    socket.join(room);
+    cb({ status: true });
+  });
+});
+
+server.listen(process.env.PORT, () => {
   console.log("Server Running at Port", process.env.PORT);
 });
+const Router = require("./src/routers/router");
+
+app.use(cors());
+// app.listen(process.env.PORT, () => {
+//   console.log("Server Running at Port", process.env.PORT);
+// });
 const jsonParser = express.json();
 // content-type = application/x-www-form-urlencoded
 // body x-www-form-urlencoded
