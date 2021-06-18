cf638a782119ae78c461f5449d8aeebfab07285f
jganz6 <jodiemanopo28@gmail.com>
2021-06-19 05:39:09 +0700

    fix update profile bug

--- src/models/users.js
+++ src/models/users.js
@@ -14,17 +14,17 @@ const getUsersInfo = (qsValue) => {
 const updateAccount = (qsValue, id) => {
   return new Promise((resolve, reject) => {
     const qs = `UPDATE tb_account SET ? WHERE id_account = ?`;
-    dbMySql.query(qs, [qsValue, id], (err, result) => {
+    dbMySql.query(qs, [qsValue, id], (err) => {
       if (err) {
         reject(err);
       } else {
         const qsUpdated =
           "SELECT id_account,username,phone_number,role,photo_profile from tb_account WHERE id_account = ?";
-        dbMySql.query(qsUpdated, qsValue, (err, data) => {
+        dbMySql.query(qsUpdated, [qsValue, id], (err, data) => {
           if (err) {
             reject(err);
           } else {
-            resolve((result = { ...result, ...data }));
+            resolve(data);
           }
         });
       }
